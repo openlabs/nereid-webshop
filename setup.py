4e17f8ea4358d03ce1e16b088c312897607a29f2
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-12-24 14:57:11 +0530

    Revert "Use data_files instead of package_data for templates"
    This reverts commit 8ab4d723e5d9d9f19043bbe7b22bd4f4b797b6c9.

--- setup.py
+++ setup.py
@@ -69,12 +69,10 @@ setup(
     package_data={
         'trytond.modules.nereid_webshop': info.get('xml', [])
         + ['tryton.cfg', 'locale/*.po', 'tests/*.rst']
-        + ['i18n/*.pot', 'i18n/pt_BR/LC_MESSAGES/*'],
+        + ['i18n/*.pot', 'i18n/pt_BR/LC_MESSAGES/*']
+        + list(get_files("templates/"))
+        + list(get_files("static/")),
     },
-    data_files=[
-        ('templates/webshop', list(get_files("templates/webshop"))),
-        ('static', list(get_files("static/"))),
-    ],
     license='GPL-3',
     install_requires=requires,
     zip_safe=False,
