2fbe355c5bbde96d20a4ec9a8bc2385a3511ace8
Prakash Pandey <prakash.pandey@openlabs.co.in>
2014-05-05 14:36:11 +0530

    Replaced urls.xml by new style route

--- setup.py
+++ setup.py
@@ -27,7 +27,9 @@ major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
-requires = []
+requires = [
+    'trytond_nereid>=3.0.7.0, <3.1',
+]
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
         requires.append(
