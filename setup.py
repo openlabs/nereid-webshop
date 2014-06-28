4ed48c7f941ef2a58560f8a7ff33e58890fea444
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-06-28 15:30:22 +0530

    Use the new description getter function
    Since nereid-catalog version 3.0.5.0 it is possible
    to set a description for the template and all the variants
    could resuse that.
    
    This getter function moves the logic of getting description
    to a method in the model.

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ minor_version = int(minor_version)
 
 requires = [
     'trytond_nereid>=3.0.8.1, <3.1',
+    'trytond_nereid_catalog>=3.0.5.0,<3.1',
 ]
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
